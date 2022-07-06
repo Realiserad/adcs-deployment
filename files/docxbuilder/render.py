bcb597cc66537bfb7f90c30e52555baba2c23c96
Bastian Fredriksson <realiserad@gmail.com>
2022-07-06 11:36:13 +0200

    Produce documentation compatible with Microsoft Word (#4)

---
+++ files/docxbuilder/render.py
@@ -0,0 +1,9 @@
+from genericpath import isfile
+from os import listdir
+from docxtpl import DocxTemplate
+import yaml
+
+document = DocxTemplate('style.docx')
+context = yaml.load(open('../../group_vars/all.yml'), Loader = yaml.FullLoader)
+document.render(context)
+document.save('../style.docx')
