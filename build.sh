47bd707d58f22d7cd7b5ee6954ec7b8bbcd61f67
Bastian Fredriksson <realiserad@gmail.com>
2022-11-06 17:35:48 +0100

    Build container images for multiple architectures (#14)

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sh container/Dockerfile.sh > Dockerfile
-docker build --squash -t realiserad/adcs-deployment .
+container/Dockerfile.sh > Dockerfile
+docker build -t realiserad/adcs-deployment .
